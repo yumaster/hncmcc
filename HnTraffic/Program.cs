3dfa6ea979a117b5c6a471a78e6798e2ae2c5e4d
ZhangYu <yumaster@yeah.net>
2020-09-21 10:48:45 +0800

    Update Program.cs

--- HnTraffic/Program.cs
+++ HnTraffic/Program.cs
@@ -14,7 +14,7 @@ namespace HnTraffic
             for (int i = 8999; i > 7999; i--)
             {
                 //paramArray.Add(new KeyValuePair<string, string>("phone", comPhone.Substring(0, 7) + i + ""));
-                paramArray.Add(new KeyValuePair<string, string>("phone", "18272609378"));
+                paramArray.Add(new KeyValuePair<string, string>("phone", "13700700000"));
                 paramArray.Add(new KeyValuePair<string, string>("comPhone", comPhone));
                 string result = HTTPClientHelper.HttpPostRequestAsync("https://h5.ha.chinamobile.com/hnmccClientWap/RecommendedGift2019_9/allrecommendx.action", paramArray, "https://h5.ha.chinamobile.com/hnmccClientWap/2019/09/recommendGift/index.html?jmphone=" + comPhone + "");
                 Console.WriteLine("Result：{0}", result);
